210602bbbcc1c7962d65cb7c1cddcd783cc029b5
AnnaDorosheva <annador@ukr.net>
2019-08-23 15:38:52 +0300

    .

--- hw-04/js/task-02.js
+++ hw-04/js/task-02.js
@@ -1,27 +1,27 @@
 'use strict'
 
 
-// const inventory = {
-//   items: ['Монорельса', 'Фильтр'],
-//   add(itemName) {
-//     this.items.push(itemName);
-//   },
-//   remove(itemName) {
-//     this.items = this.items.filter(item => item !== itemName);
-//   },
-// };
+const inventory = {
+  items: ['Монорельса', 'Фильтр'],
+  add(itemName) {
+    this.items.push(itemName);
+  },
+  remove(itemName) {
+    this.items = this.items.filter(item => item !== itemName);
+  },
+};
 
-// const invokeInventoryOperation = function(itemName, inventoryAction) {
-//   console.log(`Invoking ${inventoryAction.name} opeartion on ${itemName}`);
-//   inventoryAction.call(inventory, itemName, inventoryAction);
-// };
+const invokeInventoryOperation = function(itemName, inventoryAction) {
+  console.log(`Invoking ${inventoryAction.name} opeartion on ${itemName}`);
+  inventoryAction.call(inventory, itemName, inventoryAction);
+};
 
 
-// invokeInventoryOperation('Аптечка', inventory.add);
-// // Invoking add opeartion on Аптечка
-// console.log(inventory.items); // ['Монорельса', 'Фильтр', 'Аптечка']
-// invokeInventoryOperation('Фильтр', inventory.remove);
-// console.log(inventory.items); // ['Монорельса', 'Аптечка']
+invokeInventoryOperation('Аптечка', inventory.add);
+// Invoking add opeartion on Аптечка
+console.log(inventory.items); // ['Монорельса', 'Фильтр', 'Аптечка']
+invokeInventoryOperation('Фильтр', inventory.remove);
+console.log(inventory.items); // ['Монорельса', 'Аптечка']
 
 
 
